4c69de0380ca257a395471650898efcfc0e9f111
FizzleOut <fizzleout1337@gmail.com>
2024-11-22 19:21:36 +0100

    feat: add new Applink, AppTitle, AppSubtitle and Underlay components
    - Create an `Applink` component with gradient text and bottom border:
    - Implement gradient text and underline.
    - Add styles for normal and hover states.
    
    - Create `AppTitle` and `AppSubtitle` components:
    - Implement slots for dynamic content.
    - Add styles for titles and subtitles, including responsive styles.
    
    - Create an `Underlay` component to wrap elements:
    - Implement slots for dynamic content.
    - Add styles for the component, including rounded corners and background color.

---
+++ src/components/Base/AppLink.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+  
+</script>
+
+<template>
+  <div class="link line">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.link {
+  font-family: "Source Code Pro", monospace;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 100%;
+  transition: background 0.3s ease-in-out;
+  cursor: pointer;
+  background: var(--color-gradient);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.link:hover {
+  background: var(--color-gradient-hover);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+</style>
