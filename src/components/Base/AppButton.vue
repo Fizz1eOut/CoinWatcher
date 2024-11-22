7297405b93131a63614440e8699be5120b513c16
FizzleOut <fizzleout1337@gmail.com>
2024-11-22 18:52:14 +0100

    feat: Added a button with gradient, outline and disabled state options using Vue 3 Composition API and TypeScript
    - Defining gradient, outline and disabled props with typing and default values.
    - Creating a computed `classes` property to dynamically add classes based on props.
    - Implementing a button with slots support.
    - Adding styles to the button, including hover and disabled states, as well as different display options (gradient, outline).

---
+++ src/components/Base/AppButton.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+import { computed, defineProps } from 'vue';
+
+// Определение пропсов с типизацией
+const props = defineProps<{
+  gradient?: boolean;
+  outline?: boolean;
+  disabled?: boolean;
+}>();
+
+// Значения по умолчанию
+const gradient = props.gradient ?? false;
+const outline = props.outline ?? false;
+const disabled = props.disabled ?? false;
+
+// Вычисляемый класс
+const classes = computed(() => ({
+  'button-gradient': gradient,
+  'button-outline': outline,
+  'button-disabled': disabled,
+}));
+</script>
+
+<template>
+  <button
+    type="button"
+    class="button"
+    :class="classes"
+    :disabled="disabled"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+.button {
+  padding: 16px 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 16px;
+  font-weight: 600;
+  font-family: "Source Code Pro", monospace;
+  color: var(--color-white);
+  border: 1px solid var(--color-blue);
+  background-color: transparent;
+  border-radius: 10px;
+  transition: background-color 0.3s ease-in-out;
+  cursor: pointer;
+}
+.button:hover {
+  background-color: var(--color-blue);
+}
+.button-gradient {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+  background: var(--color-gradient);
+  cursor: pointer;
+  transition: background 0.3s ease-in-out;
+}
+.button-gradient:hover {
+  background: var(--color-gradient-hover);
+}
+.button-outline {
+  width: 39px;
+  height: 39px;
+  border-radius: 10px;
+  border: 1px solid rgba(137, 147, 158, 0.60);
+  background-color: transparent;
+  cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
+}
+.button-outline:hover {
+  background-color: var(--color-lightblue);
+}
+.button-disabled {
+  background-color: var(--color-grey);
+  opacity: 0.3;
+  transition: background-color 0.3s ease-in-out, opacity 0.3s ease-in-out;
+  cursor: not-allowed;
+}
+.button-disabled:hover {
+  opacity: 0.4;
+  background-color: var(--color-grey);
+}
+</style>
