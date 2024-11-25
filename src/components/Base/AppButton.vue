a984f75c231e121cfc54b80794483db1e1556acc
FizzleOut <fizzleout1337@gmail.com>
2024-11-25 19:35:06 +0100

    refactoring: replaced `outline` with `ordinary` and added a new style for the ordinary button
    - Renamed the "Outline" prop to "Ordinary" for a clearer and more colorful description of the style.
    - Added a new `.button-ordinary` style to support ordinary buttons.
    - Removed the `.button-outline` style as polite.
    - Computed classes updated to take into account the new style.

--- src/components/Base/AppButton.vue
+++ src/components/Base/AppButton.vue
@@ -1,28 +1,28 @@
 <script setup lang="ts">
-import { computed, defineProps } from 'vue';
+  import { computed } from 'vue';
 
-// Определение пропсов с типизацией
-const props = defineProps<{
-  gradient?: boolean;
-  outline?: boolean;
-  disabled?: boolean;
-}>();
+  // Определение пропсов с типизацией
+  const props = defineProps<{
+    gradient?: boolean;
+    ordinary?: boolean;
+    disabled?: boolean;
+  }>();
 
-// Значения по умолчанию
-const gradient = props.gradient ?? false;
-const outline = props.outline ?? false;
-const disabled = props.disabled ?? false;
+  // Значения по умолчанию
+  const gradient = props.gradient ?? false;
+  const ordinary = props.ordinary ?? false;
+  const disabled = props.disabled ?? false;
 
-// Вычисляемый класс
-const classes = computed(() => ({
-  'button-gradient': gradient,
-  'button-outline': outline,
-  'button-disabled': disabled,
-}));
+  // Вычисляемый класс
+  const classes = computed(() => ({
+    'button-gradient': gradient,
+    'button-ordinary': ordinary,
+    'button-disabled': disabled,
+  }));
 </script>
 
 <template>
-  <button
+  <button 
     type="button"
     class="button"
     :class="classes"
@@ -33,60 +33,61 @@ const classes = computed(() => ({
 </template>
 
 <style scoped>
-.button {
-  padding: 16px 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 16px;
-  font-weight: 600;
-  font-family: "Source Code Pro", monospace;
-  color: var(--color-white);
-  border: 1px solid var(--color-blue);
-  background-color: transparent;
-  border-radius: 10px;
-  transition: background-color 0.3s ease-in-out;
-  cursor: pointer;
-}
-.button:hover {
-  background-color: var(--color-blue);
-}
-.button-gradient {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 5px;
-  background: var(--color-gradient);
-  cursor: pointer;
-  transition: background 0.3s ease-in-out;
-}
-.button-gradient:hover {
-  background: var(--color-gradient-hover);
-}
-.button-outline {
-  width: 39px;
-  height: 39px;
-  border-radius: 10px;
-  border: 1px solid rgba(137, 147, 158, 0.60);
-  background-color: transparent;
-  cursor: pointer;
-  transition: background-color 0.3s ease-in-out;
-}
-.button-outline:hover {
-  background-color: var(--color-lightblue);
-}
-.button-disabled {
-  background-color: var(--color-grey);
-  opacity: 0.3;
-  transition: background-color 0.3s ease-in-out, opacity 0.3s ease-in-out;
-  cursor: not-allowed;
-}
-.button-disabled:hover {
-  opacity: 0.4;
-  background-color: var(--color-grey);
-}
+  .button {
+    padding: 16px 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 16px;
+    font-weight: 600;
+    font-family: "Source Code Pro", monospace;
+    color: var(--color-white);
+    border: 1px solid var(--color-blue);
+    background-color: transparent;
+    border-radius: 10px;
+    transition: background-color 0.3s ease-in-out;
+    cursor: pointer;
+  }
+  .button:hover {
+    background-color: var(--color-blue);
+  }
+  .button-gradient {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 5px;
+    background: var(--color-gradient);
+    cursor: pointer;
+    transition: background 0.3s ease-in-out;
+  }
+  .button-gradient:hover {
+    background: var(--color-gradient-hover);
+  }
+  .button-ordinary {
+    width: 100%;
+    height: 100%;
+    color: var(--color-white);
+    background-color: transparent;
+    cursor: pointer;
+    border-radius: 0;
+    border: none;
+    padding: 0;
+  }
+  .button-ordinary:hover {
+    background-color: transparent
+  }
+  .button-disabled {
+    background-color: var(--color-grey);
+    opacity: 0.3;
+    transition: background-color 0.3s ease-in-out, opacity 0.3s ease-in-out;
+    cursor: not-allowed;
+  }
+  .button-disabled:hover {
+    opacity: 0.4;
+    background-color: var(--color-grey);
+  }
 </style>
