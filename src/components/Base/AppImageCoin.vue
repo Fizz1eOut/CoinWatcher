a1f13244e9edecf1d9c35bfc16c9b87c1e626068
FizzleOut <fizzleout1337@gmail.com>
2024-12-07 17:04:33 +0100

    feat(components): add AppImageCoin component for displaying cryptocurrency images
    - Implemented a new `AppImageCoin` component that accepts a cryptocurrency image URL via the `imageUrl` prop.
    - Added logic for forming a full image URL given the base URL.
    - If the image URL is missing or invalid, a placeholder image with the text "Image not available" is displayed.
    - Styles set the image size (25px x 25px).
    - Simplified image processing and improved interaction with components that require displaying images.

---
+++ src/components/Base/AppImageCoin.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+  import { computed, defineProps } from 'vue';
+
+  // Интерфейс для входных параметров (пропсов)
+  interface AppImageCoinProps {
+    imageUrl: string; // URL изображения
+  }
+
+  const props = defineProps<AppImageCoinProps>();
+
+  const fullImageUrl = computed<string>(() => {
+    // Добавляем базовый URL только если imageUrl существует
+    return props.imageUrl ? 'https://cryptocompare.com' + props.imageUrl : '';
+  });
+</script>
+
+<template>
+  <img
+    v-if="fullImageUrl"
+    class="image"
+    :src="fullImageUrl"
+    alt="Image"
+  >
+  <img
+    v-else
+    class="image"
+    src="https://salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png"
+    alt="Image not available"
+  >
+</template>
+
+<style scoped>
+.image {
+  width: 25px;
+  height: 25px;
+}
+</style>
+
