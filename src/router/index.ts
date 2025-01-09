9fef8673edf7bc29fdcf5d3f037733f8e87109b0
FizzleOut <fizzleout1337@gmail.com>
2025-01-09 18:46:41 +0100

    refactor(router): updated history settings and added scroll control
    - Changed router history type from `createWebHistory` to `createWebHashHistory` with base URL `/CoinWatcher`.
    - Added scroll control via `scrollBehavior`:
    - When navigating to a new route, the page scroll is set to the top (`top: 0`).
    - When returning to the previous page, the saved scroll position (`savedPosition`) is used.
    - Updated imports to use `RouterScrollBehavior`.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
+import type { RouterScrollBehavior } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import CoinsView from '@/views/CoinsView.vue';
 import ExchangersView from '@/views/ExchangersView.vue';
@@ -7,8 +8,17 @@ import ComparisonView from '@/views/ComparisonView.vue';
 import NewsView from '@/views/NewsView.vue';
 import CoinDetailView from '@/views/CoinDetailView.vue';
 
+const scrollBehavior: RouterScrollBehavior = (to, from, savedPosition) => {
+  if (savedPosition) {
+    return savedPosition;
+  } else if (to.path !== from.path) {
+    return { top: 0 };
+  }
+  return {};
+};
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory('/CoinWatcher'),
   routes: [
     { path: '/', name: 'home', component: HomeView },
     { path: '/coins', name: 'coins', component: CoinsView },
@@ -16,9 +26,9 @@ const router = createRouter({
     { path: '/converter', name: 'converter', component: ConverterView },
     { path: '/comparison', name: 'comparison', component: ComparisonView },
     { path: '/news', name: 'news', component: NewsView },
-    { path: '/coin/:name', component: CoinDetailView, name: 'CoinDetailView',  props: true },
+    { path: '/coin/:name', component: CoinDetailView, name: 'CoinDetailView', props: true },
   ],
+  scrollBehavior,
 });
 
 export default router;
-
