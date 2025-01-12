c062c11ad9950561e7929d681a095991a7d1034f
FizzleOut <fizzleout1337@gmail.com>
2025-01-12 17:15:06 +0100

    feat: Add support for optional cryptocurrency filter to news query
    - Added `cryptoName` parameter to `getNews` function, which allows filtering news by a specific cryptocurrency.
    - If `cryptoName` is not passed, all news are returned.
    - If `cryptoName` is passed, news are filtered by the specified cryptocurrency.

--- src/api/coins/news.ts
+++ src/api/coins/news.ts
@@ -1,8 +1,13 @@
 import { fetchData } from '@/components/modules/http';
 import type { NewsResponse } from '@/interface/news.interface';
 
-// Функция для получения последних новостей
-export const getNews = async (): Promise<NewsResponse> => {
-  const url = `${import.meta.env.VITE_BASE_URL}data/v2/news/?lang=EN&api_key=${import.meta.env.VITE_API_KEY}`;
+// Функция для получения последних новостей для конкретной криптовалюты
+export const getNews = async (cryptoName?: string): Promise<NewsResponse> => {
+  const baseUrl = `${import.meta.env.VITE_BASE_URL}data/v2/news/?lang=EN&api_key=${import.meta.env.VITE_API_KEY}`;
+
+  const url = cryptoName 
+    ? `${baseUrl}&categories=${cryptoName}`
+    : baseUrl;
+
   return fetchData<NewsResponse>(url);
 };
