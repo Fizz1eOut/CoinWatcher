95031f8fd7fab57f3182095a727c688eb5fc1089
FizzleOut <fizzleout1337@gmail.com>
2025-01-16 20:11:02 +0100

    Added AppModal component for displaying modal windows
    - Created AppModal component using Teleport and Transition for rendering modal windows.
    - Support for two-way binding via prop modelValue and event update:modelValue.
    - Implemented the ability to close a modal window by clicking on the overlay using the close method.
    - Appearance:
    - Added adaptive layout for display on mobile devices.
    - Overlay style with a semi-transparent background to darken the area behind the modal window.
    - The content of the modal window is wrapped in an AppContainer with the size "md".
    - Custom slots are supported for filling the content of the modal window.

---
+++ src/components/Base/AppModal.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+  import AppContainer from '@/components/Base/AppContainer.vue';
+
+  interface AppModalProps {
+    modelValue: boolean;
+  }
+  defineProps<AppModalProps>();
+  const emits = defineEmits(['update:modelValue']);
+
+  const close = () => {
+    emits('update:modelValue', false);
+  };
+</script>
+
+<template>
+  <Teleport to="body">
+    <Transition>
+      <div v-if="modelValue" class="modal">
+        <div class="overlay" @click.self="close">
+          <div class="content">
+            <app-container size="md" class="modal-container">
+
+              <div class="content__body">
+                <slot />
+              </div>
+            </app-container>
+          </div>
+        </div>
+      </div>
+    </Transition>
+  </Teleport>
+</template>
+
+<style scoped>
+  .overlay {
+    position: fixed;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 11;
+    background-color: rgba(0, 0, 0, 0.7);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: 100%;
+  }
+  .content {
+    border-radius: 20px;
+    background: var(--color-underlay);
+    max-width: 700px;
+    width: 100%;
+  }
+  @media (max-width: 499px) {
+    .modal-container {
+      padding: 10px;
+    }
+    .content__body {
+      padding-top: 10px;
+    }
+  }
+  .v-enter-active,
+  .v-leave-active {
+    transition: opacity 0.5s ease;
+  }
+
+  .v-enter-from,
+  .v-leave-to {
+    opacity: 0;
+  }
+</style>
