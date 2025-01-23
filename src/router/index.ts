1afecec127be38998be89bdccbc7b4ba03c426ed
FizzleOut <fizzleout1337@gmail.com>
2025-01-23 19:27:36 +0100

    refactor: remove unused routes and views from router configuration
    - Removed `/converter` and `/comparison` routes from the list of routes.
    - Removed corresponding imports of `ConverterView` and `ComparisonView` components from the router.
    - Simplified the route structure by removing unused components.
    - Checked that correct navigation and scrolling behavior is preserved.

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,8 +3,6 @@ import type { RouterScrollBehavior } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import CoinsView from '@/views/CoinsView.vue';
 import ExchangersView from '@/views/ExchangersView.vue';
-import ConverterView from '@/views/ConverterView.vue';
-import ComparisonView from '@/views/ComparisonView.vue';
 import NewsView from '@/views/NewsView.vue';
 import CoinDetailView from '@/views/CoinDetailView.vue';
 import BriefcaseView from '@/views/BriefcaseView.vue';
@@ -24,8 +22,6 @@ const router = createRouter({
     { path: '/', name: 'home', component: HomeView },
     { path: '/coins', name: 'coins', component: CoinsView },
     { path: '/exchangers', name: 'exchangers', component: ExchangersView },
-    { path: '/converter', name: 'converter', component: ConverterView },
-    { path: '/comparison', name: 'comparison', component: ComparisonView },
     { path: '/news', name: 'news', component: NewsView },
     { path: '/coin/:name', component: CoinDetailView, name: 'CoinDetailView', props: true },
     { path: '/briefcase', name: 'briefcase', component: BriefcaseView },
